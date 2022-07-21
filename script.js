b21ab5e3464835c80e8db14d865923ba3e6f7e8a
Angel-Verde <verdeangel95@hotmail.com>
2022-07-21 12:53:56 -0300

    tienda

---
+++ script.js
@@ -0,0 +1,20 @@
+let nombreIngresado = prompt ("ingresa tu nombre");
+let ingresarMail = prompt ("Bienvenido a Cartagena "+nombreIngresado+" dejanos tu mail para recibir el newsletter");
+
+
+
+let products = [];
+let total = 0;
+
+
+function add(product,price) {
+    console.log (product, price);
+    products.push(product);
+    total = total + price;
+    document.getElementById("checkout").innerHTML = `Pagar $${total}`
+
+}
+alert(products.join(", \n"));
+function pagar() {
+
+}
